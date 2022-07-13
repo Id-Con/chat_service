fd90649672f0c8439ae11e14901d361bc02333fe
JwaSangHeon <whk4051@naver.com>
2022-07-13 17:14:10 +0900

    socket.io 구현중 404에러

--- SH/chat_service/app.js
+++ SH/chat_service/app.js
@@ -8,7 +8,10 @@ const socketIO = require("socket.io");
 const io = socketIO(server);
 
 app.use(express.static(path.join(__dirname, "src"))); // join은 운영체제마다 경로 표기가 다르기 때문에 사용한다.
-
 const PORT = process.env.PORT || 5000;
+
+io.on("connection", (socket) => {
+  console.log("연결이 이루어 졌습니다.");
+});
 
 app.listen(PORT, () => console.log(`server is running ${PORT}`));
